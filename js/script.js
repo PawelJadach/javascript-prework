e5505ffb5d52fc68ef45befefbebb06d84eb638f
Paweł Jadach <pawel.jadach.96@gmail.com>
2019-09-09 15:14:34 +0200

    Add full game

--- js/script.js
+++ js/script.js
@@ -1,68 +1,67 @@
+function getMoveName(argMoveId) {
+  if (argMoveId == 1) {
+    return "kamień";
+  } else if (argMoveId == 2) return "papier";
+  else if (argMoveId == 3) return "nożyczki";
+  else {
+    printMessage("Nie znam ruchu o id " + argMoveId + ".");
+    return "nieznany ruch";
+  }
+}
+function displayResult(argPlayerMove, argComputerMove) {
+  switch (argPlayerMove) {
+    case "papier":
+      if (argComputerMove == "kamień")
+        printMessage(
+          `Komputer wybrał <b>${argComputerMove}</b>, ty wybrałeś <b>${argPlayerMove}</b> więc <span class="green">wygrałeś!</span>`
+        );
+      else if (argComputerMove == "nożyczki")
+        printMessage(
+          `Komputer wybrał <b>${argComputerMove}</b>, ty wybrałeś <b>${argPlayerMove}</b> więc <span class="red">przegrałeś!</span>`
+        );
+      else
+        printMessage(
+          `Komputer wybrał <b>${argComputerMove}</b>, ty wybrałeś <b>${argPlayerMove}</b> więc remis!`
+        );
+      break;
+    case "kamień":
+      if (argComputerMove == "nożyczki")
+        printMessage(
+          `Komputer wybrał <b>${argComputerMove}</b>, ty wybrałeś <b>${argPlayerMove}</b> więc <span class="green">wygrałeś!</span>`
+        );
+      else if (argComputerMove == "papier")
+        printMessage(
+          `Komputer wybrał <b>${argComputerMove}</b>, ty wybrałeś <b>${argPlayerMove}</b> więc <span class="red">przegrałeś!</span>`
+        );
+      else
+        printMessage(
+          `Komputer wybrał <b>${argComputerMove}</b>, ty wybrałeś <b>${argPlayerMove}</b> więc remis!`
+        );
+      break;
+    case "nożyczki":
+      if (argComputerMove == "papier")
+        printMessage(
+          `Komputer wybrał <b>${argComputerMove}</b>, ty wybrałeś <b>${argPlayerMove}</b> więc <span class="green">wygrałeś!</span>`
+        );
+      else if (argComputerMove == "kamień")
+        printMessage(
+          `Komputer wybrał <b>${argComputerMove}</b>, ty wybrałeś <b>${argPlayerMove}</b> więc <span class="red">przegrałeś!</span>`
+        );
+      else
+        printMessage(
+          `Komputer wybrał <b>${argComputerMove}</b>, ty wybrałeś <b>${argPlayerMove}</b> więc remis!`
+        );
+      break;
+    case "nieznany ruch":
+      printMessage(`Wprowadziłeś złą liczbę!`);
+      break;
+  }
+}
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log("Wylosowana liczba to: " + randomNumber);
-
-let computerMove = "nieznany ruch";
-
-if (randomNumber == 1) {
-  computerMove = "kamień";
-} else if (randomNumber == 2) {
-  computerMove = "papier";
-} else computerMove = "nożyczki";
+let computerMove = getMoveName(randomNumber);
 
 let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
-let playerMove = "nieznany ruch";
-
-if (playerInput == 1) {
-  playerMove = "kamień";
-} else if (playerInput == 2) {
-  playerMove = "papier";
-} else playerMove = "nożyczki";
-
-switch (playerMove) {
-  case "papier":
-    if (computerMove == "kamień")
-      printMessage(
-        `Komputer wybrał <b>${computerMove}</b>, ty wybrałeś <b>${playerMove}</b> więc <span class="green">wygrałeś!</span>`
-      );
-    else if (computerMove == "nożyczki")
-      printMessage(
-        `Komputer wybrał <b>${computerMove}</b>, ty wybrałeś <b>${playerMove}</b> więc <span class="red">przegrałeś!</span>`
-      );
-    else
-      printMessage(
-        `Komputer wybrał <b>${computerMove}</b>, ty wybrałeś <b>${playerMove}</b> więc remis!`
-      );
-    break;
-  case "kamień":
-    if (computerMove == "nożyczki")
-      printMessage(
-        `Komputer wybrał <b>${computerMove}</b>, ty wybrałeś <b>${playerMove}</b> więc <span class="green">wygrałeś!</span>`
-      );
-    else if (computerMove == "papier")
-      printMessage(
-        `Komputer wybrał <b>${computerMove}</b>, ty wybrałeś <b>${playerMove}</b> więc <span class="red">przegrałeś!</span>`
-      );
-    else
-      printMessage(
-        `Komputer wybrał <b>${computerMove}</b>, ty wybrałeś <b>${playerMove}</b> więc remis!`
-      );
-    break;
-  case "nożyczki":
-    if (computerMove == "papier")
-      printMessage(
-        `Komputer wybrał <b>${computerMove}</b>, ty wybrałeś <b>${playerMove}</b> więc <span class="green">wygrałeś!</span>`
-      );
-    else if (computerMove == "kamień")
-      printMessage(
-        `Komputer wybrał <b>${computerMove}</b>, ty wybrałeś <b>${playerMove}</b> więc <span class="red">przegrałeś!</span>`
-      );
-    else
-      printMessage(
-        `Komputer wybrał <b>${computerMove}</b>, ty wybrałeś <b>${playerMove}</b> więc remis!`
-      );
-    break;
-  case "nieznany ruch":
-    printMessage(`Wprowadziłeś złą liczbę!`);
-    break;
-}
+let playerMove = getMoveName(playerInput);
+displayResult(playerMove, computerMove);
