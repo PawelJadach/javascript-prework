66e0bfeb7bf4361ceee51e023356551937041f1c
Paweł Jadach <pawel.jadach.96@gmail.com>
2019-09-09 14:45:48 +0200

    Add player move

--- js/script.js
+++ js/script.js
@@ -10,4 +10,11 @@ if (randomNumber == 1) {
   computerMove = "papier";
 } else computerMove = "nożyczki";
 
-printMessage("Mój ruch to: " + computerMove);
+let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
+let playerMove = "nieznany ruch";
+
+if (playerInput == 1) {
+  playerMove = "kamień";
+} else if (playerInput == 2) {
+  playerMove = "papier";
+} else playerMove = "nożyczki";
